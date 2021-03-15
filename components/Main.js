fd9c74b5e374fd49109300227d9e0645ae3113b9
Marcio Campos <cmpsmarcio@gmail.com>
2021-03-14 22:07:15 -0300

    Criação do componente dinâmicamente de acordo com o checkbox. Deleção da tarefa da lista.

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,25 @@
+import React, { useState } from 'react'
 import Wheather from './Wheather.js'
 import Exchange from './Exchange.js'
 import TaskList from './TaskList.js'
 
 export default function Main(props) {
+
+  const [wheather, setWheater] = useState(true)
+  const [exchange, setExchange] = useState(true)
+  const [tasklist, setTasklist] = useState(true)
+
+  const wheaterClick = ({ target }) => {
+    setWheater(target.checked)
+  }
+
+  const exchangeClick = ({ target }) => {
+    setExchange(target.checked)
+  }
+
+  const tasklistClick = ({ target }) => {
+    setTasklist(target.checked)
+  }
 
   return (
           <div className="flex w-full h-full">
@@ -10,28 +27,28 @@ export default function Main(props) {
               <div className="mt-4 p-8">
                 <div>
                   <label className="inline-flex items-center py-5">
-                    <input type="checkbox" className="form-checkbox h-5 w-5 text-indigo-600" />
+                    <input type="checkbox" className="form-checkbox h-5 w-5 text-indigo-600" onClick={wheaterClick} value={wheather} defaultChecked={wheather} />
                     <span className="ml-2">Previsão do Tempo</span>
                   </label>  
                 </div>
                 <div>
                   <label className="inline-flex items-center py-5">
-                  <input type="checkbox" className="form-checkbox h-5 w-5 text-green-500" />
+                  <input type="checkbox" className="form-checkbox h-5 w-5 text-green-500" onClick={exchangeClick} value={exchange} defaultChecked={exchange} />
                   <span className="ml-2">Câmbio</span>
                   </label>
                 </div>
                 <div>
                   <label className="inline-flex items-center py-5">
-                  <input type="checkbox" className="form-checkbox h-5 w-5 text-pink-600" />
+                  <input type="checkbox" className="form-checkbox h-5 w-5 text-pink-600" onClick={tasklistClick} value={tasklist} defaultChecked={tasklist} />
                   <span className="ml-2">Lista de Tarefas</span>
                   </label>
                 </div>
               </div>
             </nav>
             <div className="w-full">
-              <Wheather />
-              <Exchange />
-              <TaskList />
+              { wheather && <Wheather /> }
+              { exchange && <Exchange /> } 
+              { tasklist && <TaskList /> }
             </div>
           </div>
 )}
--- components/TaskList.js
+++ components/TaskList.js
@@ -1,31 +1,62 @@
+import React, { useState, useRef } from 'react'
+
 export default function TaskList() {
+  const inputTask = useRef(null)
+  const [tasks, setTasks] = useState([])
+  const [text, setText] = useState('')
+
+  const handleTextChanged = ({target}) => {
+    setText(target.value)
+  }
+
+  const deleteRow = ({ target }) => {
+    tasks.splice(tasks.findIndex(task => task.id == target.id), 1)
+    setTasks([...tasks])
+  }
+
+  const handleClick = () => {
+    if (text === '') {
+      return 
+    }
+    const task = {
+      id: (tasks[tasks.length - 1] || {}).id + 1 || 0,
+      description: text
+    }
+    setTasks([...tasks, task])
+    clearInputTask()
+  }
+
+  const clearInputTask = () => {
+    setText('')
+    inputTask.current.value = ''
+    inputTask.current.focus()
+  }
 
   return (
     <div className="py-2" >
       <article className="border-l-4 border-gray-400 bg-gray-100 sm:grid grid-cols-5 bg-white shadow-lg p-7 relative lg:mx-auto sm:p-4 rounded-lg lg:col-span-2 lg:ml-20" >
-        <img src="https://cdn4.iconfinder.com/data/icons/stop-virus-outline-iconset/128/ic_clipboard-128.png" alt="Just a flower" className="w-full rounded-lg" />
+        <img src="https://cdn4.iconfinder.com/data/icons/stop-virus-outline-iconset/128/ic_clipboard-128.png" alt="List" className="w-full rounded-lg" />
         <div className="pt-5 self-center sm:pt-0 sm:pl-10 col-span-3">
           <h2 className="text-gray-800 capitalize text-xl font-bold pb-4">Lista de Tarefas</h2>
           <div className="flex pb-2">
-            <input type="text" id="task" className="border border-transparent w-full focus:outline-none text-sm h-10 flex items-center" placeholder="Insira uma tarefa..." />
-            <button id="addTask" className="flex items-center justify-center h-10 w-10 mr-2  bg-gray-200 hover:bg-gray-300 text-indigo-800 text-white focus:outline-none" >+</button>
+            <input type="text" ref={inputTask} id="inputTask" className="border border-transparent w-full focus:outline-none text-sm h-10 flex items-center" placeholder="Insira uma tarefa..." onChange={handleTextChanged}/>
+            <button id="addTask" className="flex items-center justify-center h-10 w-10 mr-2  bg-gray-200 hover:bg-gray-300 text-indigo-800 text-white focus:outline-none" onClick={handleClick} >+</button>
           </div>
-          <table className="table-auto w-full text-sm">
+          <table className="table-auto w-full text-sm" >
             <thead className="italic text-xs">
               <tr>
-                <th className="w-10/12 text-left">task</th>
-                <th>id#</th>
+                <th className="w-10/12 text-left">Tarefa</th>
+                <th>#</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Tarefa 1</td>
-                <td><button className="border border-red-500 bg-red-500 text-white rounded-md px-4 py-1 m-1 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">del</button></td>
-              </tr>
-              <tr>
-                <td>Tarefa 1</td>
-                <td><button className="border border-red-500 bg-red-500 text-white rounded-md px-4 py-1 m-1 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline">del</button></td>
-              </tr>
+            {tasks.map(task => {
+              return  <tr key={task.id} >
+                        <td >{task.description}</td>
+                        <td><button id={task.id}  className="border border-red-500 bg-red-500 text-white rounded-md px-4 py-1 m-1 transition duration-500 ease select-none hover:bg-red-600 focus:outline-none focus:shadow-outline" onClick={deleteRow} >del</button></td>
+                      </tr>
+              })
+            }
             </tbody>
           </table>
         </div>
