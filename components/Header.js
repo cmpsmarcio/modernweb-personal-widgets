5eb0445f518176f3d6e5b007c41cf4acfcc10fa3
Marcio Campos <cmpsmarcio@gmail.com>
2021-03-21 16:36:25 -0300

    Adicionado tag alt faltante

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="top h-40 w-full bg-gray-100 overflow-hidden relative shadow-lg" >
     <div className="flex flex-col justify-center items-center relative h-full bg-opacity-50" >
-      <img src="https://cdn2.iconfinder.com/data/icons/shopping-elements-6/64/27-Account_and_Personal-128.png" className="h-24 w-24 object-cover rounded-full" />
+      <img src="https://cdn2.iconfinder.com/data/icons/shopping-elements-6/64/27-Account_and_Personal-128.png" alt="Foto principal" className="h-24 w-24 object-cover rounded-full" />
       <h1 className="text-2xl font-semibold">Meus Widgets</h1>  
     </div>
   </div>
